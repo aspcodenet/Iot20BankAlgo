33086374de679d2a878293945b4555fd2d39d042
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-12-02 14:33:17 +0100

    distr

--- Iot20BankAlgo/Iot20BankAlgo.cpp
+++ Iot20BankAlgo/Iot20BankAlgo.cpp
@@ -58,9 +58,15 @@ private:
 
 
 
+
+
 class VectorAccountNoPtrStorage : public IAccountStorage
 {
 public:
+	void ReserveMemory(int count)
+	{
+		accounts.reserve(count);
+	}
 	void AddAccount(std::string id) override
 	{
 		Account account = Account(id);
@@ -74,6 +80,88 @@ public:
 	}
 private:
 	std::vector<Account> accounts;
+};
+//DistributedVectorAccountStorage
+class DistributedVectorAccountStorage : public IAccountStorage
+{
+public:
+	void AddAccount(std::string id) override
+	{
+		Account account = Account(id);
+		if (id[0] == '1')
+			accountsStartingWith1.push_back(account);
+		if (id[0] == '2')
+			accountsStartingWith2.push_back(account);
+		if (id[0] == '3')
+			accountsStartingWith3.push_back(account);
+		if (id[0] == '4')
+			accountsStartingWith4.push_back(account);
+		if (id[0] == '5')
+			accountsStartingWith5.push_back(account);
+		if (id[0] == '6')
+			accountsStartingWith6.push_back(account);
+		if (id[0] == '7')
+			accountsStartingWith7.push_back(account);
+		if (id[0] == '8')
+			accountsStartingWith8.push_back(account);
+		if (id[0] == '9')
+			accountsStartingWith9.push_back(account);
+	}
+	Account* GetAccount(std::string id) override
+	{
+		int index = id[0] - '0';
+
+		switch (index)
+		{
+		case 1:
+			for (int i = 0; i < accountsStartingWith1.size(); i++)
+				if (accountsStartingWith1[i].GetId() == id) return &accountsStartingWith1[i];
+			break;
+		case 2:
+			for (int i = 0; i < accountsStartingWith2.size(); i++)
+				if (accountsStartingWith2[i].GetId() == id) return &accountsStartingWith2[i];
+			break;
+		case 3:
+			for (int i = 0; i < accountsStartingWith3.size(); i++)
+				if (accountsStartingWith3[i].GetId() == id) return &accountsStartingWith3[i];
+			break;
+		case 4:
+			for (int i = 0; i < accountsStartingWith4.size(); i++)
+				if (accountsStartingWith4[i].GetId() == id) return &accountsStartingWith4[i];
+			break;
+		case 5:
+			for (int i = 0; i < accountsStartingWith5.size(); i++)
+				if (accountsStartingWith5[i].GetId() == id) return &accountsStartingWith5[i];
+			break;
+		case 6:
+			for (int i = 0; i < accountsStartingWith6.size(); i++)
+				if (accountsStartingWith6[i].GetId() == id) return &accountsStartingWith6[i];
+			break;
+		case 7:
+			for (int i = 0; i < accountsStartingWith7.size(); i++)
+				if (accountsStartingWith7[i].GetId() == id) return &accountsStartingWith7[i];
+			break;
+		case 8:
+			for (int i = 0; i < accountsStartingWith8.size(); i++)
+				if (accountsStartingWith8[i].GetId() == id) return &accountsStartingWith8[i];
+			break;
+		case 9:
+			for (int i = 0; i < accountsStartingWith9.size(); i++)
+				if (accountsStartingWith9[i].GetId() == id) return &accountsStartingWith9[i];
+			break;
+		}
+		return nullptr;
+	}
+private:
+	std::vector<Account> accountsStartingWith1;
+	std::vector<Account> accountsStartingWith2;
+	std::vector<Account> accountsStartingWith3;
+	std::vector<Account> accountsStartingWith4;
+	std::vector<Account> accountsStartingWith5;
+	std::vector<Account> accountsStartingWith6;
+	std::vector<Account> accountsStartingWith7;
+	std::vector<Account> accountsStartingWith8;
+	std::vector<Account> accountsStartingWith9;
 };
 
 
@@ -175,14 +263,13 @@ int main()
 	//VectorAccountStorage* storage = new VectorAccountStorage();
 	VectorAccountNoPtrStorage* storage = new VectorAccountNoPtrStorage();
 	//VectorAccountMapStorage* storage = new VectorAccountMapStorage();
+	//DistributedVectorAccountStorage* storage = new DistributedVectorAccountStorage();
 	Bank* bank = new Bank(storage);
 
-	int antal = 500000;
-
+	int antal = 500;
 	start = high_resolution_clock::now();
 	for(int i = 0; i < antal;i++)
 	{
-		//Add some noice to the heap
 		bank->AddAccount(std::to_string(i));
 	}
 
